20feac8f03eaa27134e81833cb046cea8208de92
CamilaAlegre <54177449+CamilaAlegre@users.noreply.github.com>
2023-09-17 23:33:26 -0300

    Update Evento.js

--- Evento.js
+++ Evento.js
@@ -1,9 +1,14 @@
 class Evento {
-  constructor(usuario, fecha, hora, lugar, estado) {
+  constructor(usuario, emailusuario,fecha, hora, lugar, estado) {
     this.usuario = usuario;
+    this.emailusuario=emailusuario;
     this.fecha = fecha;
     this.hora = hora;
     this.lugar = lugar;
     this.estado = estado;
   }
 }
+
+
+module.exports =  Evento;
+
