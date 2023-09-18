6e644296dc50bd9d477490f20b5dc49f8d0e3153
CamilaAlegre <54177449+CamilaAlegre@users.noreply.github.com>
2023-09-17 23:34:01 -0300

    Update Loguearse.js

--- Loguearse.js
+++ Loguearse.js
@@ -1,13 +1,17 @@
+const DB_Usuarios = require("./db_Usuarios");
+
+
 class Loguearse {
-    constructor(consultaUsuario) {
-      this.consultaUsuario = consultaUsuario;
+    constructor(db_usuario) {
+      this.db_usuario = db_usuario;
     }
   
     async iniciarSesion(email, contraseña) {
-      const usuario = await this.consultaUsuario.consultarUsuario(email);
+      const usuario = await this.db_usuario.consultarUsuario(email);
   
       if (usuario && usuario.contraseña === contraseña) {
         console.log('Inicio de sesión exitoso. Usuario encontrado:');
+
         console.log(usuario);
       } else {
         console.log('Inicio de sesión fallido.');
@@ -16,10 +20,4 @@ class Loguearse {
   }
 
 
-const consultaUsuario = new DB_Usuarios();
-
-const nuevoUsuario = new Usuario('Camila Anahi', 'Alegre', 'ejemplo@example.com', 'contraseña123', 'Argentina');
-consultaUsuario.agregarUsuario(nuevoUsuario);
-
-const login = new Login(consultaUsuario);
-login.iniciarSesion('ejemplo@example.com', 'contraseña123');
+  module.exports =  Loguearse;
