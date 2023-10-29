20013d012000d39855b0891624fbb738ffd417e4
CamiAnaAle <alegrecamila1995@gmail.com>
2023-10-29 19:18:48 -0300

    Update Telegram.js

--- Telegram.js
+++ Telegram.js
@@ -2,7 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 
 class TelegramBotManager {
-  constructor(token) {
+  constructor() {
+    this.token= '6600696741:AAE2HrAyuZYJE9w1XCh24yfWsHEcagqsnlo';
     this.bot = new TelegramBot(token, { polling: false });
   }
 
@@ -15,11 +16,21 @@ class TelegramBotManager {
   sendLocation(chatId, latitude, longitude) {
     return this.bot.sendLocation(chatId, latitude, longitude);
   }
+
+
+  sendMessageAndLocationToChatIds(chatIds, evento, latitude, longitude) {
+    const promises = chatIds.map((chatId) => {
+      const message = `Hola, el usuario ${evento.usuario} le ocurrió un ${evento.estado} el ${evento.fecha} a las ${evento.hora} y se encuentra en la siguiente ubicación`;
+      return this.sendMessage(chatId, message)
+        .then(() => this.sendLocation(chatId, latitude, longitude));
+    });
+
+    return Promise.all(promises);
+  }
+
 }
 
-const token = '6600696741:AAE2HrAyuZYJE9w1XCh24yfWsHEcagqsnlo';
-
-const botManager = new TelegramBotManager(token);
+const botManager = new TelegramBotManager();
 
 const Evento = require('./Evento'); 
 
@@ -34,24 +45,14 @@ const evento = new Evento(
 
 const chatIds = ['1119289333','6308381260'];
 
-const message = 'Hola, el usuario '+evento.usuario+ ' le ocurrió un '+ evento.estado+' y se encuentra en la siguiente ubicación';
 const latitude = 37.7749;
 const longitude = -122.4194;
-// Envía el mensaje
-chatIds.forEach((chatId) => {
-  botManager.sendMessage(chatId, message)
-    .then(() => {
-      // Envía la ubicación
-      botManager.sendLocation(chatId, latitude, longitude)
-        .then(() => {
-          console.log('Ubicación enviada con éxito a ' + chatId);
-        })
-        .catch((error) => {
-          console.error('Error al enviar la ubicación a ' + chatId + ':', error);
-        });
-    })
-    .catch((error) => {
-      console.error('Error al enviar el mensaje a ' + chatId + ':', error);
-    });
+/// Envía mensajes y ubicaciones a los chatIds
+botManager.sendMessageAndLocationToChatIds(chatIds,evento, latitude, longitude)
+.then(() => {
+  console.log('Mensajes y ubicaciones enviados con éxito a los chatIds');
+})
+.catch((error) => {
+  console.error('Error al enviar mensajes y ubicaciones:', error);
 });
 
